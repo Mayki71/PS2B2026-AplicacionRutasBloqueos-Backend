05da3fbe9134b9f78a6601b71a2933088fe81269
Benjamin Calle <ceb2030005@est.univalle.edu>
2026-04-20 17:40:12 -0400

    setup modulo reports backend sprint 1 ticket 1

---
+++ src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ReportsModule } from './modules/reports/reports.module';
+
+@Module({
+  imports: [ReportsModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
+
+
