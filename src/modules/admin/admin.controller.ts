18d1ddf6a2db806abaee39455bd91ec914857b88
Jeremy <jerejosu020606@gmail.com>
2026-04-21 01:31:48 -0400

    primer commit backend admin ticket 24

---
+++ src/modules/admin/admin.controller.ts
@@ -0,0 +1,31 @@
+import { Controller, Get } from '@nestjs/common';
+import { AdminService } from './admin.service';
+import { Body, Post } from '@nestjs/common';
+
+@Controller('admin')
+export class AdminController {
+
+  constructor(private adminService: AdminService) {}
+
+  @Get('usuarios')
+  getUsuarios() {
+    return this.adminService.getUsuarios();
+  }
+
+  @Get('reportes')
+getReportes() {
+  return this.adminService.getReportes();
+}
+
+@Post('estado')
+cambiarEstado(@Body() body: any) {
+  return this.adminService.cambiarEstado(body.id, body.estado);
+}
+
+
+@Post('desactivar')
+desactivar(@Body() body: any) {
+  return this.adminService.desactivarUsuario(body.id);
+}
+
+}
