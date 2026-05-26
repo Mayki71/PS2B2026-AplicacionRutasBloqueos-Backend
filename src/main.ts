68658732563d7e694ec60626824202c8bff3c91f
SamuelAVR <vrs2029994@est.univalle.edu>
2026-05-26 18:17:08 -0400

    fix: prefijo api

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  app.setGlobalPrefix('api');
   app.enableCors({
     origin: [
       /^https?:\/\/localhost:\d+$/,
@@ -11,12 +11,15 @@ async function bootstrap() {
       /^https?:\/\/192\.168\.\d+\.\d+:\d+$/,
       /^https?:\/\/10\.\d+\.\d+\.\d+:\d+$/,
       /^https?:\/\/26\.\d+\.\d+\.\d+:\d+$/,
+      /^https:\/\/.*\.vercel\.app$/,
     ],
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
   });
+  const port = process.env.PORT || 3000;
+  await app.listen(port, '0.0.0.0');
 
-  await app.listen(3000, '0.0.0.0');
+  console.log(`Application is running on port: ${port}`);
 }
 bootstrap();
